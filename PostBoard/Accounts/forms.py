9bb878f8bbc027c3bc47cd7b611f753f2b7cbd1d
Seprent2023 <125795015+Seprent2023@users.noreply.github.com>
2024-03-24 11:11:06 +0700

    Add files via upload

--- PostBoard/Accounts/forms.py
+++ PostBoard/Accounts/forms.py
@@ -1,44 +1,83 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from allauth.account.forms import SignupForm
 from django.contrib.auth.models import User, Group
-# from django.core.mail import send_mail, mail_managers
+from django.core.mail import send_mail, mail_managers
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.auth import login, get_user_model
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.sites.models import Site
 
 from PostBoard_main.models import RegUsers
 
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(label='Email')
-
-    class Meta:
+    class Meta(UserCreationForm.Meta):
         model = User
-        fields = (
-            "username",
-            "email",
-            "password1",
-            "password2",
-        )
+        fields = UserCreationForm.Meta.fields + ('email',)
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.cleaned_data = None
+            for field in self.fields:
+                self.fields['username'].widget.attrs.update({"placeholder": 'Придумайте свой ник'})
+                self.fields['email'].widget.attrs.update({"placeholder": 'Введите свое мыло'})
+                self.fields['password1'].widget.attrs.update({"placeholder": 'Придумайте свой пароль'})
+                self.fields['password2'].widget.attrs.update({"placeholder": 'Повторите придуманный пароль'})
+                self.fields[field].widget.attrs.update({"class": "form-control", "autocomplete": "off"})
+
+        def clean_username(self):
+            username = self.cleaned_data.get('username')
+            try:
+                User.objects.get(username=username)
+                raise forms.ValidationError(
+                    self.error_messages['Пользователь существует'],
+                    code='Пользователь существует',
+                )
+            except User.DoesNotExist:
+                return username
 
 
-class CustomSignupForm(SignupForm):
+class ActivstionCodeForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super(ActivstionCodeForm, self).__init__(*args, **kwargs)
+
+    code = forms.CharField(required=True, max_length=50, label='Код подтвержения',
+                           widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+                           error_messages={'required': 'Введите код!',
+                                           'max_length': 'Максимальное количество символов 6'})
+
+    # def save(self, commit=True):
+    #     reg_user = super(ActivstionCodeForm, self).save(commit=False)
+    #     user = RegUsers.objects.create(reg_user=reg_user)
+    #     common_users = Group.objects.get(name='Зарегистрированные пользователи')
+    #     user.groups.add(common_users)
+    #     reg_user.code = self.cleaned_data['code']
+    #
+    #     if commit:
+    #         common_users = Group.objects.get(name='Зарегистрированные пользователи')
+    #         reg_user.groups.add(common_users)
+    #         reg_user.save()
+    #         return reg_user
+
     def save(self, request):
         user = super().save(request)
-        # mail_managers(
-        #     subject="Новый пользователь!",
-        #     message=f'Пользователь {user.username} зарегистрировался на сайте'
-        # )
-        #
-        # send_mail(
-        #     subject="Вуелкам!",
-        #     message=f'{user.username}, вам удалось каким то чудом зарегистрироваться, поздравляем!',
-        #     from_email=None,
-        #     recipient_list=[user.email],
-        # )
-        #
-        common_users = Group.objects.get(name='Зарегистрированные пользователи')
-        user.groups.add(common_users)
-        RegUsers.objects.create(reg_user=user)
-
+        user.code = self.cleaned_data['code']
+        user.save()
         return user
 
 
+class LoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields['username'].widget.attrs['placeholder'] = 'Логин пользователя'
+            self.fields['password'].widget.attrs['placeholder'] = 'Пароль'
+            self.fields['username'].lable = 'Логин'
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control',
+                'autocomplete': 'off'
+            })
--- PostBoard/Accounts/views.py
+++ PostBoard/Accounts/views.py
@@ -1,24 +1,115 @@
-from django.contrib.auth.models import User
-from django.views.generic.edit import CreateView
-from .forms import SignUpForm
-# from PostBoard_main.models import RegUsers
-# from django.shortcuts import redirect, render, get_object_or_404
+import random
+
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User, Group
+from django.contrib.auth.views import LoginView
+# from django.views.generic.edit import CreateView
+from django.views.generic import CreateView, TemplateView
+from .forms import SignUpForm, ActivstionCodeForm, LoginForm
+from django.core.mail import send_mail
+from PostBoard_main.models import RegUsers
+from django.shortcuts import redirect, render
+
+
+def generate_code():
+    random.seed()
+    return str(random.randint(100000, 999999))
 
 
 class SignUp(CreateView):
-    model = User
     form_class = SignUpForm
-    success_url = '/Accounts/login'
-    template_name = 'registration/signup.html'
+    success_url = '/accounts/login'
+    template_name = 'registration/user_register.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Регистрация на сайте'
+        return context
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.is_active = False
+        user.save()
+        username = form.cleaned_data.get('username')
+        email = form.cleaned_data.get('email')
+        my_password1 = form.cleaned_data.get('password1')
+        u_f = User.objects.get(username=username, email=email, is_active=False)
+        code = generate_code()
+        if RegUsers.objects.filter(code=code):
+            code = generate_code()
+
+        messages = code
+        user = authenticate(username=username, email=email, password=my_password1)
+        reg_user = RegUsers.objects.create(reg_user=u_f, code=code)
+
+        send_mail(subject='Код подтверждения',
+                  message=f'{username}! Для подтверждения введите код: {messages}, вы {reg_user, user}',
+                  from_email=None, recipient_list=[email], fail_silently=False)
+
+        return redirect('email_confirmation_sent')
 
 
-# def register(request):
-#     if request.methof == 'POST':
-#         user_form = SignUpForm(request.POST)
-#         if user_form.is_valid():
-#             new_user = user_form.save(commit=False)
-#             RegUsers.objects.create(reg_user=new_user)
-#             common_users = Group.objects.get(name='Зарегистрированные пользователи')
-#             new_user.groups.add(common_users)
-#             new_user.save()
-#             return redirect(f'/posts/')
+def user_confirm_email_view(request):
+    if request.method == 'POST':
+        form = ActivstionCodeForm(request.POST)
+        if form.is_valid():
+            code_use = form.cleaned_data.get('code')
+            if RegUsers.objects.filter(code=code_use):
+                reg_user_code = RegUsers.objects.get(code=code_use)
+            else:
+                form.add_error(None, 'Код подтверждения не совпадает.')
+                return render(request, 'registration/email_confirmation_sent.html', {'form': form})
+            if reg_user_code.reg_user.is_active == False:
+                common_users = Group.objects.get(name='Зарегистрированные пользователи')
+                reg_user_code.reg_user.groups.add(common_users)
+                reg_user_code.reg_user.is_active = True
+                reg_user_code.reg_user.save()
+                login(request, reg_user_code.reg_user, backend='django.contrib.auth.backends.ModelBackend')
+                RegUsers.objects.all().update(code=None)
+                return redirect('email_confirmed')
+            else:
+                form.add_error(None, 'Вы кто!?')
+                return render(request, 'registration/email_confirmation_sent.html', {'form': form})
+        else:
+            return render(request, 'registration/email_confirmation_sent.html', {'form': form})
+    else:
+        form = ActivstionCodeForm()
+        return render(request, 'registration/email_confirmation_sent.html', {'form': form})
+
+
+class EmailConfirmationSentView(TemplateView):
+    template_name = 'registration/email_confirmation_sent.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Письмо активации отправлено'
+        return context
+
+
+class EmailConfirmedView(TemplateView):
+    template_name = 'registration/email_confirmed.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Ваш электронный адрес активирован'
+        return context
+
+
+class EmailConfirmationFailedView(TemplateView):
+    template_name = 'registration/email_confirmation_failed.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Ваш электронный адрес не активирован'
+        return context
+
+
+class CustomLoginView(LoginView):
+    form_class = LoginForm
+    template_name = 'registration/user_login.html'
+    next_page = 'posts'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Авторизация на сайте'
+        return context
